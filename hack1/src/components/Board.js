d268855fbf04dfb66e76eed886cb86f2d240b58c
Bosh-Kuo <evaktm1680@gmail.com>
2021-11-02 12:23:01 +0800

    upload hack1

---
+++ hack1/src/components/Board.js
@@ -0,0 +1,84 @@
+/****************************************************************************
+  FileName      [ Board.js ]
+  PackageName   [ src/components ]
+  Author        [ Cheng-Hua Lu, Chin-Yi Cheng ]
+  Synopsis      [ This file generates the Board. ]
+  Copyright     [ 2021 10 ]
+****************************************************************************/
+
+import React, { useEffect, useState } from 'react';
+import Cell from './Cell';
+import Modal from './Modal';
+import Dashboard from './Dashboard';
+import createBoard from '../util/createBoard';
+import { revealed } from '../util/reveal';
+import './css/Board.css'
+
+
+const Board = ({ boardSize, mineNum, backToHome }) => {
+    const [board, setBoard] = useState([]);                     // An 2-dimentional array. It is used to store the board.
+    const [nonMineCount, setNonMineCount] = useState(0);        // An integer variable to store the number of cells whose value are not '💣'.
+    const [mineLocations, setMineLocations] = useState([]);     // An array to store all the coordinate of '💣'.
+    const [gameOver, setGameOver] = useState(false);            // A boolean variable. If true, means you lose the game (Game over).
+    const [remainFlagNum, setRemainFlagNum] = useState(0);      // An integer variable to store the number of remain flags.
+    const [win, setWin] = useState(false);                      // A boolean variable. If true, means that you win the game.
+
+    useEffect(() => {
+        // Calling the function
+        freshBoard();
+    }, []);
+
+    // Creating a board
+    const freshBoard = () => {
+        {/* -- TODO 3-1 -- */ }
+        {/* Useful Hint: createBoard(...) */ }
+
+        setBoard(createBoard(boardSize, mineNum).board);
+        setBoard(createBoard(boardSize, mineNum).mineLocations);
+
+    }
+
+    const restartGame = () => {
+        {/* -- TODO 5-2 -- */ }
+        {/* Useful Hint: freshBoard() */ }
+        freshBoard();
+    }
+
+    // On Right Click / Flag Cell
+    const updateFlag = (e, x, y) => {
+        // To not have a dropdown on right click
+        e.preventDefault();
+        // Deep copy of a state
+        {/* -- TODO 3-2 -- */ }
+        {/* Useful Hint: A cell is going to be flagged. 'x' and 'y' are the xy-coordinate of the cell. */ }
+        {/* Reminder: If the cell is already flagged, you should unflagged it. Also remember to update the board and the remainFlagNum. */ }
+        {/* Reminder: The cell can be flagged only when it is not revealed. */ }
+
+    };
+
+    const revealCell = (x, y) => {
+        {/* -- TODO 4-1 -- */ }
+        {/* Reveal the cell */ }
+        {/* Useful Hint: The function in reveal.js may be useful. You should consider if the cell you want to reveal is a location of mines or not. */ }
+        {/* Reminder: Also remember to handle the condition that after you reveal this cell then you win the game. */ }
+
+    };
+
+    return (
+
+        <div className='boardPage' >
+            <div className='boardWrapper' >
+                {/* <h1 >This is the board Page!</h1>  This line of code is just for testing. Please delete it if you finish this function. */}
+                {/* -- TODO 3-1 -- */}
+                {/* Useful Hint: The board is composed of BOARDSIZE*BOARDSIZE of Cell (2-dimention). So, nested 'map' is needed to implement the board.  */}
+                {/* Reminder: Remember to use the component <Cell> and <Dashboard>. See Cell.js and Dashboard.js for detailed information. */}
+                <div className="boardContainer"></div>
+            </div>
+        </div>
+    );
+
+
+
+}
+
+export default Board
---
+++ hack1/src/util/createBoard.js
@@ -0,0 +1,107 @@
+/****************************************************************************
+  FileName      [ createBoard.js ]
+  PackageName   [ src/util ]
+  Author        [ Cheng-Hua Lu, Chin-Yi Cheng ]
+  Synopsis      [ This file generates the pattern of mines and the board. ]
+  Copyright     [ 2021 10 ]
+****************************************************************************/
+
+import randomNum from "./randomFixSeed";
+
+export default (boardSize, mineNum) => {
+    let board = [];
+    let mineLocations = [];
+
+    // Print Board function (For testing)
+    const printBoard = () => {
+        console.log("Current Board")
+        for(let x = 0; x < boardSize; x++){
+            console.log(board[x].map((x) => {
+                return(x.value !=='💣' ? x.value.toString()+" " : x.value)
+            }))
+        }
+    }
+
+    // Create a blank board
+    for(let x = 0; x < boardSize; x++){
+        let subCol = [];
+        for(let y = 0; y < boardSize; y++){
+            subCol.push({
+                value: 0,                   // To store the number of mines around the cell.
+                revealed: false,            // To store if the cell is revealed.
+                x: x,                       // To store the x coordinate (the column index) of the cell.
+                y: y,                       // To store the y coordinate (the row index) of the cell.
+                flagged: false,             // To store if the cell is flagged.
+            });
+        }
+        board.push(subCol);
+    }
+    
+    // Random bombs locations
+    let mineCount = 0;
+    while(mineCount < mineNum){
+        let x = randomNum(0, boardSize - 1);
+        let y = randomNum(0, boardSize - 1);
+
+        if(board[x][y].value === 0){            // Check this location has not been located a mine.
+            board[x][y].value = '💣';           // Change the value of the cell to '💣'
+            mineLocations.push([x, y]);
+            mineCount++;
+        }
+    }
+
+    {/* -- TODO 2 -- */}
+    {/* Useful Hints: Calculate and update the value of each cell in the board. The value means the number of mines adjacent to the cell. */}
+    {/* Reminder: Some cells in the board do not have "Top" position, some do not have "Top-Right" position .... */}
+    {/* Warning: The value of any cell will not be bigger than 8 logically. */}
+    {/* Testing: printBoard() */}
+    function getLocation(x,y,i){
+        if(i==0){
+            return {"X":x-1, "Y":y-1};
+        }else if(i==1){
+            return {"X":x, "Y":y-1};
+        }else if (i==2){
+            return {"X":x+1, "Y":y-1};
+        }else if (i==3){
+            return {"X":x+1, "Y":y};
+        }else if (i==4){
+            return {"X":x+1, "Y":y+1};
+        }else if (i==5){
+            return {"X":x, "Y":y+1};
+        }else if (i==6){
+            return {"X":x-1, "Y":y+1};
+        }else if (i==7){
+            return {"X":x-1, "Y":y};
+        }
+    }
+
+    for(let x = 0; x < boardSize; x++){
+        for(let y = 0; y < boardSize; y++){
+            // console.log("yes")
+            if (board[x][y].value == '💣'){
+                continue;
+            }
+
+            let bump = 0;
+
+            for(let i = 0; i<8; i++){
+                const location = getLocation(x,y,i);
+                let locX = location.X;
+                let locY = location.Y;
+                if (locX>=boardSize || locX<0 || locY>=boardSize || locY<0){
+
+                    continue;
+                }else{
+                    if (board[locX][locY].value == '💣'){
+                        bump+=1;
+                    }
+                }
+            }
+    
+            board[x][y].value = bump;
+        }
+    }
+
+    printBoard()
+    return { board, mineLocations };
+};
